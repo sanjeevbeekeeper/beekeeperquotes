eda52214459d052c3bf79f338681abb331b33e9b
Sanjeev Beekeeper PC home <sanjeevramanathan@gmail.com>
2017-03-16 07:33:15 +0530

    Updated with class.

--- 404.php
+++ 404.php
@@ -1,8 +1,14 @@
 <?php get_header(); ?>
 <!-- header -->
 
-<h1>404 Page Not Found</h1>
-<p>We are sorry the page you are looking for does not exist.</p>
+<div class="quotes_container">
+
+    <div class="error_container text-center">
+        <h1>404 Page <br>Not Found</h1>
+        <p>We are sorry the page you are looking for does not exist.</p>
+    </div>
+
+</div> <!-- // .quotes_container -->
 
 <!-- footer -->
 <?php get_footer(); ?>
