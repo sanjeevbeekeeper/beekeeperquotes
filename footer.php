f580da042a36038ad53ee96ff9f973a601ba6324
Sanjeev Beekeeper PC home <sanjeevramanathan@gmail.com>
2017-03-16 07:33:31 +0530

    Updated with correct link.

--- footer.php
+++ footer.php
@@ -2,20 +2,20 @@
 
     <!-- footer -->
         <footer>
+
             <!-- title -->
             <div class="footer_blogtitle">
-                <a href="<?php the_permalink(); ?>"> <?php bloginfo('name'); ?> </a>
-
+                <a href="<?php echo home_url(); ?>"> <?php bloginfo('name'); ?> </a>
             </div>
 
-            <!-- browse by author -->
+            <!-- Total no of Quotes -->
             <div class="footer_browsebyauthor">
                 <span>50 Quotes</span>
                 <!-- <a href="#">Browse by Author</a> -->
             </div>
 
             <div class="footer_copyright">
-                2005 - <?php echo date('Y'); ?> &copy; <?php bloginfo('name'); ?>. <span class="nowrap">Created by <a href="http://sanjeevbeekeeper.com">Sanjeev Beekeeper</a></span>
+                2005 - <?php echo date('Y'); ?> &copy; <?php bloginfo('name'); ?>. <span class="nowrap">Created by <a href="http://sanjeevbeekeeper.com" target="_blank">Sanjeev Beekeeper</a></span>
             </div>
         </footer>
     </div>
