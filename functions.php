866714798a40e60deac3afa1a90f6fd6e3e69027
Sanjeev Beekeeper PC home <sanjeevramanathan@gmail.com>
2017-03-25 22:31:14 +0530

    Included remove_emoji.php in the functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+// functions are in 'inc/' folder 
 require get_template_directory() . '/inc/cleanup.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/quotes_settings.php';
+require get_template_directory() . '/inc/remove_emoji.php'; // 25 mar 2017
