a7ba5e32488b24a798d4372dd0086584cfd1b176
Sanjeev Beekeeper PC home <sanjeevramanathan@gmail.com>
2017-07-15 15:35:30 +0530

    Added the wp_bootstrap_navwalker class

--- functions.php
+++ functions.php
@@ -1,7 +1,19 @@
 <?php
 
-// functions are in 'inc/' folder 
+// functions are in 'inc/' folder
 require get_template_directory() . '/inc/cleanup.php';
 require get_template_directory() . '/inc/enqueue.php';
 require get_template_directory() . '/inc/quotes_settings.php';
 require get_template_directory() . '/inc/remove_emoji.php'; // 25 mar 2017
+
+/* =================================================
+    PLUGIN: wp-bootstrap-navwalker
+==================================================== */
+//// Register Custom Navigation Walker
+require_once('wp_bootstrap_navwalker.php');
+//// Declaring the menu
+register_nav_menus( array(
+    'primary-menu' => __( 'Primary Menu', 'THEMENAME' ),
+    ));
+
+    
