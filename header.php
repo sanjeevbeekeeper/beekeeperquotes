c08a3c03009ba1172f8d028093db03157f69b4e2
Sanjeev Beekeeper PC home <sanjeevramanathan@gmail.com>
2017-07-15 15:35:56 +0530

    Blog title is added with the conditional tag

--- header.php
+++ header.php
@@ -12,3 +12,22 @@
 
         <!-- content wrapping -->
         <div class="container minheight">
+
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
+                    <div id="pagename">
+                        <!-- If is_home then make the title simple, else make it clickable link -->
+                        <?php
+                        if ( is_home() ) {
+                            ?>
+                            <h3> <?php bloginfo('name'); ?> </h3>
+                        <?php }
+                        else {
+                            ?>
+                            <h4> <a href="<?php echo home_url(); ?>"> Back </a> </h4>
+                        <?php }
+                            ?>
+
+                    </div> <!-- #pagename -->
+                </div> <!-- col -->
+            </div> <!-- row -->
